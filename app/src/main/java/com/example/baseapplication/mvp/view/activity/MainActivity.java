5033599af9b7d082a978ae73bcf3c4be9a14336a
xujun <xvjun@haotang365.com.cn>
2019-10-14 09:50:38 +0800

    1

--- app/src/main/java/com/example/baseapplication/mvp/view/activity/MainActivity.java
+++ app/src/main/java/com/example/baseapplication/mvp/view/activity/MainActivity.java
@@ -1,15 +1,59 @@
 package com.example.baseapplication.mvp.view.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.example.baseapplication.R;
+import com.example.baseapplication.mvp.presenter.base.BasePresenter;
+import com.example.baseapplication.mvp.view.activity.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * 首页
+ */
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutResID() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void setView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initEvent() {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected boolean isUseEventBus() {
+        return false;
+    }
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setSwipeBack(false);
     }
 }
